695c39c2dffa12dd498abf85f9efb8058538a682
Khalid Abdi <xamza2009@gmail.com>
2025-12-01 19:56:18 +0300

    data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world! khalid");
+    //dwa
+    let x:i32 =10;
+    let y:u64=30;
+    let n:f32=6.94;
+    println!("the first number is {}",x);
+    println!("the second number is {}",y);
+     println!("the second number is {}",n);
+
+    let is_snow:bool=true;
+    println!("is ?{}",is_snow);
+
+    let c:char='k';
+    println!("{}",c);
+    println!("{}",x);
 }
