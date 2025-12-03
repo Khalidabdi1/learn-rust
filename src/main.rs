bb9a43a8aa2ad6c431bc3478ba6accd9ee7f6765
Khalid Abdi <xamza2009@gmail.com>
2025-12-03 21:36:29 +0300

    IF

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 
 fn main() {
-//comment
+// condition (IF)
+let age:i32=15;
 
-// single commit
-/*
-mul line
-*/
+if age<18{
+    println!("you can drive")
+}else {
+    println!("tou can't drive")
+}
+
+
+let odd:i32=14;
+
+if odd%2==0{
+    println!("is even")
+}else if odd%2!=0 {
+    println!("id odd")
+}
+
+
+let num:bool=true;
+let check=if num {5} else {1};
+println!("{check}");
+
 
 
 
