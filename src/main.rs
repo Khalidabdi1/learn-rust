f999bf0a3010e6bb54cd9ad70444858aa44d83ec
Khalid Abdi <xamza2009@gmail.com>
2025-12-03 21:19:21 +0300

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
 
 fn main() {
-//shadowing
-let x:i32= 5;
-let x:i32=x+1;
-{
+//comment
 
-    let x:i32=x*2;
-    println!("the value is x{}",x);
-}
+// single commit
+/*
+mul line
+*/
 
-println!("the value of x is {}",x);
-
-//mutable
-let mut y:i32=7;
-y=10;
-println!("the value of x is {}",y);
-
-let space:&str="";
-println!("the lenth is {}",space.len())
 
 
 
